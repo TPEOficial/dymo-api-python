a542eeb5c14bac7b9b84ee59aa967fad6896d668
lizard <lizardwine@hotmail.com>
2024-09-01 17:16:39 -0600

    ♻️ Adding types to function responses

--- dymoapi/response_models.py
+++ dymoapi/response_models.py
@@ -1,6 +1,67 @@
 from pydantic import BaseModel
+from typing import List
 
 class UrlEncryptResponse(BaseModel):
     original: str
     code: str
     encrypt: str
+
+class IsValidPwdDetails(BaseModel):
+    validation: str
+    message: str
+
+class IsValidPwdResponse(BaseModel):
+    valid: bool
+    password: str
+    details: List[IsValidPwdDetails]
+
+
+class SatinizerFormats(BaseModel):
+    ascii: bool
+    bitcoinAddress: bool
+    cLikeIdentifier: bool
+    coordinates: bool
+    crediCard: bool
+    date: bool
+    discordUsername: bool
+    doi: bool
+    domain: bool
+    e164Phone: bool
+    email: bool
+    emoji: bool
+    hanUnification: bool
+    hashtag: bool
+    hyphenWordBreak: bool
+    ipv6: bool
+    ip: bool
+    jiraTicket: bool
+    macAddress: bool
+    name: bool
+    number: bool
+    panFromGstin: bool
+    password: bool
+    port: bool
+    tel: bool
+    text: bool
+    semver: bool
+    ssn: bool
+    uuid: bool
+    url: bool
+    urlSlug: bool
+    username: bool
+
+class SatinizerIncludes(BaseModel):
+    spaces: bool
+    hasSql: bool
+    hasNoSql: bool
+    letters: bool
+    uppercase: bool
+    lowercase: bool
+    symbols: bool
+    digits: bool
+
+
+class SatinizerResponse(BaseModel):
+    input: str
+    formats: SatinizerFormats
+    includes: SatinizerIncludes
--- tests/test_dymoapi.py
+++ tests/test_dymoapi.py
@@ -22,21 +22,21 @@ class TestPasswordValidation(unittest.TestCase):
         assert self.client.is_valid_pwd({
                 "password": "123456",
                 "min": 16
-            })["valid"] == False, "Password is valid, expected invalid."
+            }).valid == False, "Password is valid, expected invalid."
         assert self.client.is_valid_pwd({
                 "email": "username@test.com",
                 "password": "Username123",
-            })["valid"] == False, "Password is valid, expected invalid."
+            }).valid == False, "Password is valid, expected invalid."
         assert self.client.is_valid_pwd({
                 "email": "username@test.com",
                 "password": "TheMegaPassword_safe512*",
                 "min": 16
-            })["valid"] == True, "Password is invalid, expected valid."
+            }).valid == True, "Password is invalid, expected valid."
         assert self.client.is_valid_pwd({
                 "email": "username@test.com",
                 "password": "TheMegaPassword_safe512*UltraLongPassword",
                 "max": 32   
-            })["valid"] == False, "Password is valid, expected invalid."
+            }).valid == False, "Password is valid, expected invalid."
         
 
 class TestInputSatinizer(unittest.TestCase):
@@ -47,8 +47,8 @@ class TestInputSatinizer(unittest.TestCase):
         self.client = DymoAPI(self.config)
     
     def test_features(self):
-        assert self.client.satinizer({"input": "' OR '1'='1' --"})["includes"]["hasSql"] == True, "Input is not sanitized."
-        assert self.client.satinizer({"input": '{"id": {"$eq": 1}}'})["includes"]["hasNoSql"] == True, "Input is not sanitized."
-        assert self.client.satinizer({"input": "https://example.com"})["formats"]["url"] == True, "Input is not sanitized."
-        assert self.client.satinizer({"input": "192.168.100.1"})["formats"]["ip"] == True, "Input is not sanitized."
+        assert self.client.satinizer({"input": "' OR '1'='1' --"}).includes.hasSql == True, "Input is not sanitized."
+        assert self.client.satinizer({"input": '{"id": {"$eq": 1}}'}).includes.hasNoSql == True, "Input is not sanitized."
+        assert self.client.satinizer({"input": "https://example.com"}).formats.url == True, "Input is not sanitized."
+        assert self.client.satinizer({"input": "192.168.100.1"}).formats.ip == True, "Input is not sanitized."
 if __name__ == "__main__": unittest.main()
