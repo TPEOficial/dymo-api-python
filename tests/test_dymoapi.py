b5d5f752c0c01083297afa2c381dfdb4f29c51c7
lizard <lizardwine@hotmail.com>
2024-09-01 15:08:33 -0600

    🔨 Fixed module import error at dymoapi/dymoapi.py line 22

--- tests/test_dymoapi.py
+++ tests/test_dymoapi.py
@@ -1,7 +1,7 @@
 import unittest
-from dymoapi import DymoAPI
+from dymoapi.dymoapi import DymoAPI
 
-class TestDymoAPI(unittest.TestCase):
+class TestUrlEncrypt(unittest.TestCase):
     def setUp(self):
         self.config = {
             # "api_key": "PRIVATE_TOKEN_HERE"
@@ -9,6 +9,46 @@ class TestDymoAPI(unittest.TestCase):
         self.client = DymoAPI(self.config)
     
     def test_features(self):
-        print(self.client.new_url_encrypt("https://dymo.tpeoficial.com").encrypt)
+        assert self.client.new_url_encrypt("https://example.com").encrypt.startswith("https://api.tpeoficial.com/public/url-encrypt/")
 
+class TestPasswordValidation(unittest.TestCase):
+    def setUp(self):
+        self.config = {
+            # "api_key": "PRIVATE_TOKEN_HERE"
+        }
+        self.client = DymoAPI(self.config)
+    
+    def test_features(self):
+        assert self.client.is_valid_pwd({
+                "password": "123456",
+                "min": 16
+            })["valid"] == False, "Password is valid, expected invalid."
+        assert self.client.is_valid_pwd({
+                "email": "username@test.com",
+                "password": "Username123",
+            })["valid"] == False, "Password is valid, expected invalid."
+        assert self.client.is_valid_pwd({
+                "email": "username@test.com",
+                "password": "TheMegaPassword_safe512*",
+                "min": 16
+            })["valid"] == True, "Password is invalid, expected valid."
+        assert self.client.is_valid_pwd({
+                "email": "username@test.com",
+                "password": "TheMegaPassword_safe512*UltraLongPassword",
+                "max": 32   
+            })["valid"] == False, "Password is valid, expected invalid."
+        
+
+class TestInputSatinizer(unittest.TestCase):
+    def setUp(self):
+        self.config = {
+            # "api_key": "PRIVATE_TOKEN_HERE"
+        }
+        self.client = DymoAPI(self.config)
+    
+    def test_features(self):
+        assert self.client.satinizer({"input": "' OR '1'='1' --"})["includes"]["hasSql"] == True, "Input is not sanitized."
+        assert self.client.satinizer({"input": '{"id": {"$eq": 1}}'})["includes"]["hasNoSql"] == True, "Input is not sanitized."
+        assert self.client.satinizer({"input": "https://example.com"})["formats"]["url"] == True, "Input is not sanitized."
+        assert self.client.satinizer({"input": "192.168.100.1"})["formats"]["ip"] == True, "Input is not sanitized."
 if __name__ == "__main__": unittest.main()
